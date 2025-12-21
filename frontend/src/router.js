7302727a70e067255da9843b43713d8449c74532
C-Ford17 <83851979+C-Ford17@users.noreply.github.com>
2025-12-21 13:57:00 -0500

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,13 +3,16 @@ import HomeView from './components/HomeView.vue';
 import MenuView from './components/MenuView.vue';
 import PedidoStatus from './components/PedidoStatus.vue';
 import MesasQR from './components/MesasQR.vue';
-
+import CuentaView from './views/CuentaView.vue';
 const routes = [
   { path: '/', component: HomeView },
   { path: '/menu', component: MenuView },
   { path: '/pedido/:id/status', component: PedidoStatus },
   { path: '/mesa/:id', component: PedidoStatus },
-  { path: '/mesas-qr', name: 'mesas-qr', component: MesasQR }
+  { path: '/mesas-qr', name: 'mesas-qr', component: MesasQR },
+  { path: '/cuenta/:id', name: 'cuenta', component: CuenvaView },
+}
+
 ];
 
 const router = createRouter({
